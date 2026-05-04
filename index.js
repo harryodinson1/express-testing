77c61827ca7b19c3c2c53b7ce9ef4b8e1219d643
harryodinson1 <harryodinson369@gmail.com>
2026-05-04 21:08:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => {
     res.status(200).json({ status: "It's working!!!" });
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000..."));
+app.listen(8080, () => console.log("Server is running on port 3000..."));
